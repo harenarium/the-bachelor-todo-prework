0ab5c38f1471d37e647baf02d9f1c490b5d52e04
Caroline Lee <carolinel2610@gmail.com>
2018-01-17 05:43:19 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,3 +1,4 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
   name = ""
   data[season].each {|hash_in_a|
@@ -43,6 +44,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
+  binding.pry
   age_sum = 0
   counter = 0
   data.each{|season,a|
