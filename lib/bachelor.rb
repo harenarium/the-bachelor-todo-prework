1730182abe7b0d537b97762463b27e2aca545645
Caroline Lee <carolinel2610@gmail.com>
2018-01-17 05:23:11 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,15 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.each{|season,a|
+    season.each {|hash_in_a|
+      if hash_in_a["occupation"]==occupation
+        name = hash_in_a["name"]
+      end
+    }
+  }
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
